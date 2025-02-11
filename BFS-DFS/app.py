c11f8557e1c1775daf773f76ea711fd2d8402ce2
duravkin <aduravkin04@gmail.com>
2025-02-11 11:23:00 +0300

    Декомпозиция. Добавление функции удаления.

--- BFS-DFS/app.py
+++ BFS-DFS/app.py
@@ -9,11 +9,13 @@ class GraphApp:
         self.Radius = 15
 
         self.nodes = dict()
-        self.edges = dict()
+        self.edges = list()
         self.selected_node = None
+        self.mode = 'N'
 
-        self.canvas.bind("<Button-1>", self.add_node)
-        self.canvas.bind("<B1-Motion>", self.move_node)
+        self.canvas.bind("<Button-1>", self.left_click)
+        self.canvas.bind("<B1-Motion>", self.move_mouse)
+        self.canvas.bind("<Button-3>", self.right_click)
 
     def check_node(self, x, y):
         overlapping = self.canvas.find_overlapping(x - 1, y - 1, x + 1, y + 1)
@@ -21,10 +23,28 @@ class GraphApp:
             return overlapping[0]
         return None
 
-    def add_node(self, event):
+    def left_click(self, event):
+        x, y = event.x, event.y
+        if self.mode == 'N':
+            self.add_node(x, y)
+
+    def move_mouse(self, event):
         x, y = event.x, event.y
         R = self.Radius
 
+        # Сброс выбора узла, если кнопка мыши отпущена
+        if event.state == 0:
+            self.selected_node = None
+        elif self.mode == 'N':
+            self.move_node(x, y)
+
+    def right_click(self, event):
+        x, y = event.x, event.y
+        if self.mode == 'N':
+            self.delete_node(x, y)
+
+    def add_node(self, x, y):
+        R = self.Radius
         # Проверяем, не находится ли новая вершина рядом с другими
         current_node = self.check_node(x, y)
         if current_node is None:
@@ -39,18 +59,18 @@ class GraphApp:
             # Если вершина уже есть, выбираем ее
             self.selected_node = current_node
 
-    def move_node(self, event):
-        x, y = event.x, event.y
-        R = self.Radius
-
-        # Сброс выбора узла, если кнопка мыши отпущена
-        if event.state == 0:
-            self.selected_node = None
-
+    def move_node(self, x, y):
         # Если узел уже выбран, перемещаем его
-        elif self.selected_node is not None:
+        if self.selected_node is not None:
+            R = self.Radius
             self.canvas.coords(self.selected_node, x - R, y - R, x + R, y + R)
             self.canvas.coords(self.nodes[self.selected_node][1], x, y)
+
+    def delete_node(self, x, y):
+        node = self.check_node(x, y)
+        if node in self.nodes:
+            self.canvas.delete(node)
+            self.nodes.pop(node)
 
 
 if __name__ == "__main__":
